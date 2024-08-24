a2c9c9574be9d5142df684d3a4f7d2ff4355bed6
Natthakorn007 <bornbmi@gmail.com>
2024-08-24 15:00:54 +0700

    Update formadd.js

--- hendle f.ts/formadd.js
+++ hendle f.ts/formadd.js
@@ -1,5 +1,4 @@
 function formSend(event) {
-<<<<<<< HEAD
      event.preventDefault();
  const username = event.target.elements["username"].value;
  const password = event.target.elements["password"].value;
@@ -11,19 +10,10 @@ function formSend(event) {
  const height = event.target.elements["height"].value;
  const weight = event.target.elements["weight"].value;
  const data = {
-=======
-    event.preventDefault();
-    const username = event.target.elements["username"].value;
-    const password = event.target.elements["password"].value;
-    const firstname = event.target.elements["firstname"].value;
-    const lastname = event.target.elements["lastname"].value;
-    const data = {
->>>>>>> 6db2ef2e5bff7381550355b1015db10a5ddca705
         username: username,
         password: password,
         firstname: firstname,
         lastname: lastname,
-<<<<<<< HEAD
         dateOfBirth: dateOfBirth,
         gender: gender,
         email: email,
@@ -31,9 +21,5 @@ function formSend(event) {
         weight: weight,
      };
      console.log(data);
-=======
     };
-    console.log(data);
->>>>>>> 6db2ef2e5bff7381550355b1015db10a5ddca705
-}
 
