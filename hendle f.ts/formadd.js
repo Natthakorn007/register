30845836f94929545dcc2cfdc6304fd04f46994a
Natthakorn007 <bornbmi@gmail.com>
2024-08-24 14:53:06 +0700

    new

--- hendle f.ts/formadd.js
+++ hendle f.ts/formadd.js
@@ -1,14 +1,24 @@
 function formSend(event) {
-     event.prevenDefault();
-     const username = event.target.elements["username"].value;
-     const password = event.target.elements["password"].value;
-     const firstname = event.target.elements["firstname"].value;
-     const lastname = event.target.elements["lastname"].value;
-     const data = {
-          username: username,
-          password: password,
-          firstname: firstname,
-          lastname: lastname,
+     event.preventDefault();
+ const username = event.target.elements["username"].value;
+ const password = event.target.elements["password"].value;
+ const firstname = event.target.elements["firstname"].value;
+ const lastname = event.target.elements["lastname"].value;
+ const dateOfBirth = event.target.elements["date_of_birth"].value;
+ const gender = event.target.elements["gender"].value;
+ const email = event.target.elements["email"].value;
+ const height = event.target.elements["height"].value;
+ const weight = event.target.elements["weight"].value;
+ const data = {
+        username: username,
+        password: password,
+        firstname: firstname,
+        lastname: lastname,
+        dateOfBirth: dateOfBirth,
+        gender: gender,
+        email: email,
+        height: height,
+        weight: weight,
      };
      console.log(data);
 }
